f172320ce0aae99f60bb9dcc2a3acb857ba965ec
Don Noska <noska.don@gmail.com>
2022-10-04 21:42:15 -0500

    navigation part 1

--- screens/MainComponent.js
+++ screens/MainComponent.js
@@ -1,15 +1,43 @@
-import { useState } from "react";
-import { View } from "react-native";
-import { CAMPSITES } from "../shared/campsites";
+import { Platform, View } from "react-native";
 import CampsiteInfoScreen from "./CampsiteInfoScreen";
 import DirectoryScreen from "./DirectoryScreen";
+import Constants from "expo-constants";
+import { createStackNavigator } from "@react-navigation/stack";
 
-const Main = () => {
-	const [campsites, setCampsites] = useState(CAMPSITES);
-	const [selectedCampsiteId, setSelectedCampsiteId] = useState();
+const DirectoryNavigator = () => {
+	const Stack = createStackNavigator();
 
 	return (
-		<View style={{ flex: 1 }}>
+		<Stack.Navigator
+			initialRouteName="Directory"
+			screenOptions={{
+				headerStyle: {
+					backgroundColor: "#5637DD",
+				},
+				headerTintColor: "#fff",
+			}}
+		>
+			<Stack.Screen
+				name="Directory"
+				component={DirectoryScreen}
+				options={{ title: "Campsite Directory" }}
+			/>
+
+			<Stack.Screen
+				name="CampsiteInfo"
+				component={CampsiteInfoScreen}
+				options={({ route }) => ({ title: route.params.campsite.name })}
+			/>
+		</Stack.Navigator>
+	);
+};
+
+const Main = () => {
+	return (
+		<View
+			style={{ flex: 1 }}
+			paddingTop={Platform.OS === "ios" ? 0 : Constants.statusBarHeight}
+		>
 			<DirectoryScreen
 				campsites={campsites}
 				onPress={(campsiteId) => setSelectedCampsiteId(campsiteId)}
